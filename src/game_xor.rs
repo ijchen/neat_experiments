f86ed493d63255fd57130ea3505260a97c91ad23
Isaac Chen <isaacjchen1@gmail.com>
2022-08-14 21:27:07 -0400

    Minor comment wording update

--- src/game_xor.rs
+++ src/game_xor.rs
@@ -15,14 +15,14 @@ impl Environment for GameXor {
         let mut scores: Vec<f64> = vec![];
 
         for predictor in population {
-            // Ensure the neural network and this game are on the same page
+            // Ensure the predictor and this game are on the same page
             // about how many inputs and outputs there should be
             debug_assert!(predictor.input_count() == self.input_count());
             debug_assert!(predictor.output_count() == self.output_count());
 
             // For something as simple as XOR, we can test all possible inputs
             // Overfitting is not a concern here, since the point isn't to generalize,
-            // but to test the network's ability to learn non-linear functions
+            // but to test the predictor's ability to learn non-linear functions
             let mut score = 0.0;
             score += 1.0 - (predictor.predict(vec![0.0, 0.0])[0] - 0.0).abs();
             score += 1.0 - (predictor.predict(vec![0.0, 1.0])[0] - 1.0).abs();
